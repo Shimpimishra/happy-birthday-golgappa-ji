67cdbeb7bc8f20fa50d69335eb732d60f6fa551b
Shimpimishra <85190437+Shimpimishra@users.noreply.github.com>
2026-06-28 23:54:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,32 @@
-window.onload=()=>{
-
-setTimeout(()=>{
-
-alert("Happy Birthday My Golgappa ❤️\n\nLove You Forever\n\n- Your Rasmalai");
-
-},2000);
-
-}
-
-document.querySelectorAll("img").forEach(img=>{
-
-img.addEventListener("click",()=>{
-
-img.classList.toggle("zoom");
-
-});
-
-});
-
 const canvas=document.createElement("canvas");
-
 document.body.appendChild(canvas);
 
 const ctx=canvas.getContext("2d");
 
+function resize(){
 canvas.width=window.innerWidth;
-
 canvas.height=window.innerHeight;
+}
+
+resize();
+window.addEventListener("resize",resize);
 
 let hearts=[];
 
-for(let i=0;i<40;i++){
-
+for(let i=0;i<80;i++){
 hearts.push({
-
 x:Math.random()*canvas.width,
-
 y:Math.random()*canvas.height,
-
 size:Math.random()*6+3,
-
-speed:Math.random()*1+0.3
-
+speed:Math.random()*1+0.4
 });
-
 }
 
 function draw(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="#ff6fb5";
+ctx.fillStyle="#ff6aa8";
 
 hearts.forEach(h=>{
 
@@ -63,11 +39,8 @@ ctx.fill();
 h.y-=h.speed;
 
 if(h.y<0){
-
-h.y=canvas.height;
-
+h.y=canvas.height+20;
 h.x=Math.random()*canvas.width;
-
 }
 
 });
